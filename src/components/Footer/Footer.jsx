164915b0c5749fe6fc9b9324d380da2bc5c8bf5c
Jy714 <yongjeremy0714@gmail.com>
2024-06-27 18:04:39 +0800

    finish

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import youtube_icon from "../../assets/youtube_icon.png"
 import twitter_icon from "../../assets/twitter_icon.png"
 import instagram_icon from "../../assets/instagram_icon.png"
 import facebook_icon from "../../assets/facebook_icon.png"
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import search_icon from "../../assets/search_icon.svg"
 import bell_icon from "../../assets/bell_icon.svg"
 import profile_img from "../../assets/profile_img.png"
 import caret_icon from "../../assets/caret_icon.svg"
-import { logout } from "../../firebase";
+import { auth, logout } from "../../firebase";
 
 const Navbar = () => {
 
@@ -38,7 +38,7 @@ const Navbar = () => {
       </div>
       <div className="navbar-right">
         <img src={search_icon} alt="" className="icons" />
-        <p>Children</p>
+        <p>{auth.currentUser.email}</p>
         <img src={bell_icon} alt="" className="icons" />
         <div className="navbar-profile">
           <img src={profile_img} alt="" className="profile" />
